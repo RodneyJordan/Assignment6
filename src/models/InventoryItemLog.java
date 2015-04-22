cd8218fe7d7ffe0ee000f6a1e302905b57f6a98f
Rodney Jordan <rodneycjordan@gmail.com>
2015-04-21 19:58:04 -0500

    added table model and updates to item log

--- src/models/InventoryItemLog.java
+++ src/models/InventoryItemLog.java
@@ -1,5 +1,49 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.Date;
+
+
 public class InventoryItemLog {
+	
+	/**
+	 * A log entry
+	 */
+	private LogEntry logEntry;
+	
+	/**
+	 * A list of log entries
+	 */
+	ArrayList<LogEntry> entries = new ArrayList<LogEntry>();
+	
+	/**
+	 * Constructor
+	 */
+	public InventoryItemLog(Date date, String description) {
+		logEntry = new LogEntry(date, description);
+		entries.add(logEntry);
+	}
+	
+	/**
+	 * Add log entry to list
+	 */
+	public void addLogEntry(Date date, String description) {
+		logEntry = new LogEntry(date, description);
+		entries.add(logEntry);
+	}
+	
+	/**
+	 * Get log entry by date
+	 */
+	public LogEntry getLogEntryByDate(Date date) {
+		logEntry = null;
+		for(int i = 0; i < entries.size(); i++) {
+			if(date.equals(entries.get(i).getDate())) {
+				logEntry = entries.get(i);
+				break;
+			}
+		}
+		return logEntry;
+	}
 
 }
---
+++ src/models/ItemLogTableModel.java
@@ -0,0 +1,51 @@
+package models;
+
+import java.util.ArrayList;
+
+import javax.swing.table.AbstractTableModel;
+
+@SuppressWarnings("serial")
+public class ItemLogTableModel extends AbstractTableModel {
+	
+	/**
+	 * Column Names
+	 */
+	private String[] columnNames = {"Date", "Description"};
+	
+	/**
+	 * ArrayList to display
+	 */
+	private ArrayList<LogEntry> logEntries;
+	
+	/**
+	 * Constructor
+	 */
+	public ItemLogTableModel(ArrayList<LogEntry> logEntries) {
+		this.logEntries = logEntries;
+	}
+	
+
+	@Override
+	public int getRowCount() {
+		return this.logEntries.size();
+	}
+
+	@Override
+	public int getColumnCount() {
+		return this.columnNames.length;
+	}
+
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		LogEntry l = logEntries.get(rowIndex);
+		if(columnIndex == 0) {
+			String date = l.getDate().toString();
+			return date;
+		}
+		else if(columnIndex == 1) {
+			return l.getDescription();
+		}
+		return null;
+	}
+
+}
